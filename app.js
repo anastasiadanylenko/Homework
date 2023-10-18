3a4c1de0e847076b5ace280e4cdfb5ff93ae344b
anastasiadanylenko <136683214+anastasiadanylenko@users.noreply.github.com>
2023-10-18 15:07:55 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 "use strict"
 
 let number1 = 2;
-let number2 =4;
+let number2 = 4;
 
+let result = number1 + number2;
+console.log(result);
+
+result = number1 - number2;
+console.log(result);
+
+result = number1 * number2;
+console.log(result);
+
+result = number1 / number2;
+console.log(result);
+
+result = number1 ** 2;
+console.log(result);
+
+let squareRoot = Math.sqrt(number2);
+console.log(squareRoot);
