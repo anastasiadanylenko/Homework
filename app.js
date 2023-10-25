e891791830903a82d5b726ece67f188a3bbdf54d
anastasiadanylenko <136683214+anastasiadanylenko@users.noreply.github.com>
2023-10-25 14:02:51 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 "use strict"
+for (let x = 1; x <= 100; x++) { 
+if (x % 3 == 0 && x % 5 == 0) { 
+console.log("ЛолКек");
+} else if (x % 3 == 0) { 
+    console.log("ЛоЛ");
+} else if (x % 5 == 0) { 
+console.log("КeК");
+} else {
+    console.log(x);
+} 
+}
 
-let number1 = 2;
-let number2 = 4;
 
-let result = number1 + number2;
-console.log(result);
+let value1 = 50;
+if (typeof value1 != "number") {
+    console.log("Таке чуство шо Бог десь наказує нас за шось"); 
+} else {
+  for (let a = 2; a < value1; a++) {
+ if (a % 2 === 0) {
+console.log(a);  
+}
+  }
+}
 
-result = number1 - number2;
-console.log(result);
 
-result = number1 * number2;
-console.log(result);
-
-result = number1 / number2;
-console.log(result);
-
-result = number1 ** 2;
-console.log(result);
-
-let squareRoot = Math.sqrt(number2);
-console.log(squareRoot);
-
-let animal = "cat";
-let animalString = animal.toString();
-console.log(animalString);
-
-let animalNumber = +animal;
-console.log(animalNumber);
-
-let animalBool = !!animal;
-console.log(animalBool);
+ let value2 = 44;
+ if (typeof value2 != "number") {
+    console.log("Таке чуство шо Бог десь наказує нас за шось");
+} else {
+let b = 2; 
+while ( b < value2) {
+ if  (b % 2 === 0) {
+   console.log(b);
+}
+b++;
+ }
+}
