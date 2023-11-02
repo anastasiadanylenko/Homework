5878b49695df1cb9c7b1c9ce891f4e31f4d2c88d
anastasiadanylenko <136683214+anastasiadanylenko@users.noreply.github.com>
2023-11-03 00:01:18 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,60 @@
 "use strict"
 
-let number1 = 2;
-let number2 = 4;
+const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
 
-let result = number1 + number2;
-console.log(result);
+let initials = userNames.map(name => {
 
-result = number1 - number2;
-console.log(result);
+let parts = name.split(' '); 
+let newInitials = parts.map(part => part[0]).join('.'); 
 
-result = number1 * number2;
-console.log(result);
+return newInitials;
+});
 
-result = number1 / number2;
-console.log(result);
+console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
 
-result = number1 ** 2;
-console.log(result);
 
-let squareRoot = Math.sqrt(number2);
-console.log(squareRoot);
 
-let animal = "cat";
-let animalString = animal.toString();
-console.log(animalString);
 
-let animalNumber = +animal;
-console.log(animalNumber);
+const userNames1 = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
 
-let animalBool = !!animal;
-console.log(animalBool);
+const arr = 'АЕІОУЯЮЄ';
+
+let filteredNames = userNames1.filter(userName => {
+return arr.includes(userName[0]) 
+});
+
+console.log(filteredNames); // ['Емма', 'Юстин', 'Ілля', 'Яна', 'Антон', 'Олена']
+
+filteredNames = []; 
+for (let i = 0; i < userNames1.length; i++) {
+if (arr.includes(userNames1[i][0])) {
+    filteredNames.push(userNames1[i]);
+}
+}
+
+console.log(filteredNames); // ['Емма', 'Юстин', 'Ілля', 'Яна', 'Антон', 'Олена']
+
+
+
+
+const currentMaxValue = 4589;
+
+let reverseMaxValue = currentMaxValue.toString().split("").reverse().join("");
+
+reverseMaxValue = +reverseMaxValue;
+
+console.log(reverseMaxValue); // 9854
+console.log(typeof reverseMaxValue); // 'number'
+
+
+
+
+let resultsArray = [1, 2, [3, [4]]];
+
+resultsArray = resultsArray.flat(Infinity);
+
+   let productOfArray = resultsArray.reduce(function(accumulator, cur, index, arr) {
+    return accumulator * cur;
+    })
+ 
+console.log(productOfArray); // 24//
