d3f421b944c09cab14713de992bc87ee5dcaa93e
anastasiadanylenko <136683214+anastasiadanylenko@users.noreply.github.com>
2023-12-05 00:09:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,74 @@
 "use strict"
 
-let number1 = 2;
-let number2 = 4;
+const button = document.querySelector('.button');
 
-let result = number1 + number2;
-console.log(result);
+function handleClick(event) {
 
-result = number1 - number2;
-console.log(result);
+    const buttonText = event.target.textContent;
+    const container = document.querySelector('.container');
+    const dateElement = document.querySelector('.date');
+    const storageButtonStatus = localStorage.getItem('buttonStatus');
+    const storageLastTurnOnStatus = localStorage.getItem('lastTurnOn');
+    const storageLastTurnOffStatus = localStorage.getItem('lastTurnOff');
 
-result = number1 * number2;
-console.log(result);
+    if (buttonText === 'Turn off') {
+        event.target.textContent = 'Turn on';
+        localStorage.setItem('buttonStatus', 'Turn On');
+        const date = new Date();
+        dateElement.textContent = `Last turn off ${date}`;
+        localStorage.setItem('lastTurnOff', date);
+        dateElement.style.color = "white";
+        container.style.backgroundColor = 'black';
+    } else {
+        event.target.textContent = 'Turn off';
+        localStorage.setItem('buttonStatus', 'Turn Off');
+        const date2 = new Date();
+        dateElement.textContent = `Last turn on ${date2}`;
+        localStorage.setItem('lastTurnOn', date2);
+        dateElement.style.color = "black";
+        container.style.backgroundColor = 'white';
 
-result = number1 / number2;
-console.log(result);
+        if (storageButtonStatus === null) {
+            localStorage.setItem('buttonStatus', 'Turn On');
+            localStorage.setItem('lastTurnOn', date2);
+        }
+    }
+}
 
-result = number1 ** 2;
-console.log(result);
+function updateDisplay() {
 
-let squareRoot = Math.sqrt(number2);
-console.log(squareRoot);
+    const button = document.querySelector('.button');
+    const container = document.querySelector('.container');
+    const dateElement = document.querySelector('.date');
 
-let animal = "cat";
-let animalString = animal.toString();
-console.log(animalString);
+    const storageButtonStatus = localStorage.getItem('buttonStatus');
+    const storageLastTurnOnStatus = localStorage.getItem('lastTurnOn');
+    const storageLastTurnOffStatus = localStorage.getItem('lastTurnOff');
 
-let animalNumber = +animal;
-console.log(animalNumber);
+    if (storageButtonStatus === 'Turn On') {
+        button.textContent = 'Turn on'
+        dateElement.style.color = "white";
+        container.style.backgroundColor = 'black';
 
-let animalBool = !!animal;
-console.log(animalBool);
+        if (storageLastTurnOffStatus) {
+            const date = new Date(storageLastTurnOffStatus);
+            dateElement.textContent = `Last turn off: ${date}`;
+        }
+
+    } else {
+        button.textContent = 'Turn off'
+        dateElement.style.color = "black";
+        container.style.backgroundColor = 'white';
+    }
+
+    if (storageLastTurnOnStatus) {
+        const date = new Date(storageLastTurnOnStatus);
+        dateElement.textContent = `Last turn on: ${date}`;
+    }
+}
+
+
+button.addEventListener('click', handleClick);
+updateDisplay();
+
+
